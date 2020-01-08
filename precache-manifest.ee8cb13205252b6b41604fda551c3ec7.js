dabc7cfa4a4c2116458981d27b21b692f3586c27
Alan Morgan <alanxoc3@gmail.com>
2020-01-07 21:43:00 -0700

    New build on Tue 07 Jan 2020 09:43:00 PM MST

---
+++ precache-manifest.ee8cb13205252b6b41604fda551c3ec7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2536782270132a5294d68a974bba269f",
+    "url": "/charagen/index.html"
+  },
+  {
+    "revision": "38d9c2dd4ffad880e9de",
+    "url": "/charagen/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e9ce0d575f5ae1af86ce",
+    "url": "/charagen/static/js/2.bc119e53.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/charagen/static/js/2.bc119e53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38d9c2dd4ffad880e9de",
+    "url": "/charagen/static/js/main.c5360e71.chunk.js"
+  },
+  {
+    "revision": "1cdc75e012f5c2eee343",
+    "url": "/charagen/static/js/runtime-main.81d36029.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/charagen/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b5ce868951fa7e45f43265ac38fbcc8.js"
+  "/charagen/precache-manifest.ee8cb13205252b6b41604fda551c3ec7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/charagen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
