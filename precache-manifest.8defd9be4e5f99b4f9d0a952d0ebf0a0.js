9bc398c6e98e01a8e40430f9db7085212529208b
Alan Morgan <alanxoc3@gmail.com>
2020-02-02 21:54:27 -0700

    New build on Sun 02 Feb 2020 09:54:27 PM MST

---
+++ precache-manifest.8defd9be4e5f99b4f9d0a952d0ebf0a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3ca557d16ebf7f6fe73a6d6abe5cee",
+    "url": "/charagen/index.html"
+  },
+  {
+    "revision": "a08cfd28805465bfb60e",
+    "url": "/charagen/static/css/main.8e2c08bb.chunk.css"
+  },
+  {
+    "revision": "72371994a2e379887ca2",
+    "url": "/charagen/static/js/2.bb57741b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/charagen/static/js/2.bb57741b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a08cfd28805465bfb60e",
+    "url": "/charagen/static/js/main.f66ce9f1.chunk.js"
+  },
+  {
+    "revision": "1cdc75e012f5c2eee343",
+    "url": "/charagen/static/js/runtime-main.81d36029.js"
+  }
+]);
